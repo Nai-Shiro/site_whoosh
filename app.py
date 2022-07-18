13e470c1bc0ad75a174bed917a2a94d640ff0dda
NaiShiro <zyckov.vad@yandex.ru>
2022-07-18 21:04:45 +0300

    1

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ def index():
 
 
 def main():  # Функция запуска
-    app.run(port=$PORT)
+    import os
+    ON_HEROKU = os.environ.get('ON_HEROKU')
+    if ON_HEROKU:
+        port = int(os.environ.get("PORT", 17995))
+    else:
+        port = 3000
+    app.run(port=port)
 
 
 main()
