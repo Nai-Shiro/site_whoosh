b28ae754b00699bbd7536cf8987274e8b02189dd
NaiShiro <zyckov.vad@yandex.ru>
2022-07-18 21:18:19 +0300

    1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn runp-heroku:app
+web: gunicorn -b 0.0.0.0:$PORT runp-heroku:app
--- app.py
+++ app.py
@@ -1,22 +1,9 @@
-from flask import Flask, render_template, redirect
-
-
+from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return "Hello, World!"
+def hello_world():
+    return 'Hello World!'
 
-
-def main():  # Функция запуска
-    import os
-    ON_HEROKU = os.environ.get('ON_HEROKU')
-    if ON_HEROKU:
-        port = int(os.environ.get("PORT", 17995))
-    else:
-        port = 3000
-    app.run(port=port)
-
-
-main()
+if __name__ == '__main__':
+    app.run()
